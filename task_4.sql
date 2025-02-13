35b2d11898af6f3f2d056cb607dc907eecf55ebb
Awill-McOloo-jr <williamstom242@gmail.com>
2025-02-13 06:12:32 +0300

    Added task 4

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Use the specified database
+USE alx_book_store;
+
+-- Query the INFORMATION_SCHEMA to get the table structure of 'Books'
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
